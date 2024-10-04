0de923f654a0d92848e43505523ef3fb7a0a24e7
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-03 21:14:53 -0500

    249. Creando los botones de Editar y Eliminar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -8,13 +8,27 @@ type PatientDetailsProps = {
 export default function PatientDetails({patient} : PatientDetailsProps) {
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
-
             <PatientDetailItem label="ID" data={patient.id} />
             <PatientDetailItem label="Nombre" data={patient.name} />
             <PatientDetailItem label="Propietario" data={patient.caretaker} />
             <PatientDetailItem label="Email" data={patient.email} />
             <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
             <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+            
+            <div className="flex justify-between mt-10">
+                <button
+                    type="button" 
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg"
+                >
+                    Editar
+                </button>
+                <button
+                    type="button" 
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg"
+                >
+                    Eliminar
+                </button>
+            </div>
         </div>
     )
 }
